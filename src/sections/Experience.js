646d5a43d366188debffe7115e79c37edda57425
Maria Arevalo <mariaisabelarevalo@hotmail.com>
2020-06-08 00:57:48 -0500

    Fixed typos

--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -44,7 +44,7 @@ const Background = () => (
   </div>
 );
 
-const CARD_HEIGHT = "270px";
+const CARD_HEIGHT = "300px";
 
 const MEDIA_QUERY_SMALL = "@media (max-width: 400px)";
 
@@ -84,6 +84,7 @@ const ProjectTag = styled.div`
     top: calc(-${CARD_HEIGHT} - 3.5px + (${CARD_HEIGHT} / 4));
   }
 `;
+
 const Project = ({
   name,
   description,
@@ -101,9 +102,12 @@ const Project = ({
             {name}
           </Title>
         </span>
-        <Text width={[1]} style={{ overflow: "auto" }} color="text">
-          {description}
-        </Text>
+        <Text
+          width={[1]}
+          style={{ overflow: "auto" }}
+          color="text"
+          dangerouslySetInnerHTML={createMarkup(description)}
+        ></Text>
       </TextContainer>
       <ProjectTag>
         <Flex>
@@ -122,6 +126,8 @@ const Project = ({
     </Flex>
   </Card>
 );
+
+const createMarkup = (htmlText) => ({ __html: htmlText });
 
 Project.propTypes = {
   name: PropTypes.string.isRequired,
@@ -143,7 +149,7 @@ const experienceEn = [
     id: "1",
     name: "Polyglot SAS",
     description:
-      "As a Full stack developer I helped to build custom web platforms using web frameworks such as Angular 4+, Nest.js, Django, Ionic and Flutter.  We also used SCRUM framework for developing said products.",
+      "As a Full stack developer I helped to build custom web platforms using web frameworks such as <strong> Angular 4+, Nest.js, Django, Ionic and Flutter </strong>.  We also used SCRUM framework for developing said products.",
     url: "https://polyglot.site/",
     urlName: "See website",
     type: "WEB & Mobile",
@@ -157,7 +163,7 @@ const experienceEn = [
     id: "2",
     name: "Muevelo dashboard",
     description:
-      'The data that was gathered with the "Muévelo App" was stored in a NoSQL DB. I implemented an ETL to process the data, according to the requirements, and load it into a PostgreSQL DB. Finally, I developed a web application built with Flash and Dash to visualize and interact with the data. ',
+      'The data that was gathered with the "Muévelo App" was stored in a NoSQL DB. I implemented an ETL in <strong>python</strong> to process the data, according to the requirements, and load it into a PostgreSQL DB. Finally, I developed a web application built with Flash and Dash to visualize and interact with the data.',
     url: "https://www.frontiersin.org/articles/10.3389/fpubh.2020.00064/full",
     urlName: "See study",
     type: "WEB & data analysis",
@@ -171,7 +177,7 @@ const experienceEn = [
     id: "3",
     name: "Múevelo App",
     description:
-      "Developed an android application to track location of participants from the TrUST study for the Faculty of Medicine. The app was build with Kotlin and was connected to Firebase to store its data in a NoSQL database. It had an special focus on compatibility in a wide range of devices as well as battery optimization.",
+      'Developed an android application to track location of participants from the TrUST study for the Faculty of Medicine from <a target="_blank" style="text-decoration: none" href="https://uniandes.edu.co/en"> Universidad de Los Andes</a>. The app was build with Kotlin and was connected to Firebase to store its data in a NoSQL database. It had an special focus on compatibility in a wide range of devices as well as battery optimization.',
     url: "https://www.frontiersin.org/articles/10.3389/fpubh.2020.00064/full",
     urlName: "See study",
     type: "Mobile",
@@ -185,7 +191,7 @@ const experienceEn = [
     id: "4",
     name: "Acsendo",
     description:
-      "As an IT intern, I was in charge of tracking reports on team development and timing. I was also in charge of keeping an eye on task distribution and bug fixes. In addition, I made tests and small adjustments to a product under development built with Angular 2 and Java. All this using the SCRUM methodology.",
+      "As an IT intern, I was in charge of tracking reports on IT's team development and timing. I was also in charge of keeping an eye on task distribution and bug fixes. In addition, I made tests and small adjustments to a product under development built with Angular 2 and Java. All this using the SCRUM methodology.",
     url: "https://www.acsendo.com/",
     urlName: "See website",
     type: "Web",
@@ -230,7 +236,7 @@ const experienceEs = [
     id: "3",
     name: "Múevelo App",
     description:
-      "Desarrollé una aplicación Android para rastrear la ubicación de los participantes del estudio TrUST para la Facultad de Medicina. La aplicación se creó con Kotlin y se conectó a Firebase para almacenar sus datos en una base de datos NoSQL. Tenía un enfoque especial en la compatibilidad en una amplia gama de dispositivos, así como en la optimización de la batería.",
+      'Desarrollé una aplicación Android para rastrear la ubicación de los participantes del estudio TrUST para la Facultad de Medicina de la <a target="_blank" style="text-decoration: none" href="https://uniandes.edu.co/en"> Universidad de Los Andes</a>. La aplicación se creó con Kotlin y se conectó a Firebase para almacenar sus datos en una base de datos NoSQL. Tenía un enfoque especial en la compatibilidad en una amplia gama de dispositivos, así como en la optimización de la batería.',
     url: "https://www.frontiersin.org/articles/10.3389/fpubh.2020.00064/full",
     urlName: "Ver estudio",
     type: "Mobile",
@@ -244,7 +250,7 @@ const experienceEs = [
     id: "4",
     name: "Acsendo",
     description:
-      "Como pasante de TI, estaba a cargo de hacer informes de seguimiento sobre el desarrollo y los tiempos del equipo. Junto a esto, también me encargaba de estar pendiente de distribución de tareas y solución de bugs. Además, hacía pruebas y ajustes pequeños a un producto en desarrollo construido con Angular 2. Todo esto usando la metodología SCRUM",
+      "Como pasante de TI, estaba a cargo de hacer informes de seguimiento sobre el desarrollo y los tiempos del equipo de TI. Junto a esto, también me encargaba de estar pendiente de distribución de tareas y solución de bugs. Además, hacía pruebas y ajustes pequeños a un producto en desarrollo construido con Angular 2. Todo esto usando la metodología SCRUM",
     url: "https://www.acsendo.com/",
     urlName: "Ver sitio",
     type: "Web",
@@ -258,7 +264,6 @@ const experienceEs = [
 
 const Experience = () => {
   const intl = useIntl();
-  console.log(intl.locale);
   const experience = intl.locale === "en" ? experienceEn : experienceEs;
   return (
     <Section.Container
